0689912fba4ff8aa0a6e4b43e99617350165da86
VageeshaDeSilva <suvadesilva@gmail.com>
2024-05-18 15:21:29 +0530

    home page spacing fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 function Home() {
   return (
     <>
-      <section className="sm:mt-6 lg:mt-8 mt-12 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <section className="sm:mt-6 lg:mt-5 mt-8 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 
         <div
-          className="my-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28 flex gap-3 lg:flex-justify lg:flex flex-col lg:flex-row">
+          className="my-5 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-10 lg:px-8 xl:mt-10 flex gap-3 lg:flex-justify lg:flex flex-col lg:flex-row">
           <div className="sm:text-center lg:text-left">
             <h1 className="text-4xl tracking-tight font-extrabold text-gray-800 sm:text-5xl md:text-6xl">
               <span className="block xl:inline">Make Your own</span><br />
